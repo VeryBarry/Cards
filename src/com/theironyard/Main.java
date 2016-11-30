9a6fb7495b5939b35792767f6032752c2feea65c
VeryBarry <barry.a.daniels@gmail.com>
2016-11-30 10:29:22 -0500

    does all the things

--- src/com/theironyard/Main.java
+++ src/com/theironyard/Main.java
@@ -1,6 +1,6 @@
 package com.theironyard;
 
-import java.util.HashSet;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class Main {
@@ -49,10 +49,92 @@ public class Main {
         return suits.size() == 1;
     }
 
+    public static boolean isPair (HashSet<Card> hand) {
+        HashMap<Card.Rank, Integer> ranks = new HashMap<>();
+        for (Card c : hand) {
+            if (!ranks.containsKey(c.rank)) {
+                ranks.put(c.rank, 1);
+            }
+            else {
+                int tally = ranks.get(c.rank);
+                int newTally = tally + 1;
+                ranks.put(c.rank, newTally);
+            }
+        }
+        return ranks.containsValue(2);
+    }
+
+    public static boolean isStraight(HashSet<Card> hand) {
+        HashSet<Card.Rank> ranks = new HashSet<>();
+        ArrayList<Integer> rankOrdinalArray = new ArrayList<>();
+        for (Card c : hand) {
+            ranks.add(c.rank);
+            rankOrdinalArray.add(c.rank.ordinal());
+        }
+        Collections.sort(rankOrdinalArray);
+        int consecutive = 0;
+        ArrayList<Integer> aceHighStraightArray = new ArrayList<Integer> (Arrays.asList(10, 11, 12, 0));
+        for (int i = 1; i < ranks.size(); i++) {
+            boolean straight = false;
+            boolean aceHighStraight = false;
+            if (rankOrdinalArray.get(i) - rankOrdinalArray.get(i - 1) <= 1) {
+                consecutive++;
+                if (consecutive >= 3) {
+                    straight = true;
+                }
+            }
+            if (rankOrdinalArray.containsAll(aceHighStraightArray)) {
+                aceHighStraight = true;
+            }
+            if (straight || aceHighStraight) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean threeOfAKind (HashSet<Card> hand) {
+        HashMap<Card.Rank, Integer> ranks = new HashMap<>();
+        for (Card c : hand) {
+            if (!ranks.containsKey(c.rank)) {
+                ranks.put(c.rank, 1);
+            }
+            else {
+                int tally = ranks.get(c.rank);
+                int newTally = tally + 1;
+                ranks.put(c.rank, newTally);
+            }
+        }
+        return ranks.containsValue(3);
+    }
+
+    public static boolean fourOfAKind (HashSet<Card> hand) {
+        HashSet<Card.Rank> ranks = new HashSet<>();
+        for (Card c : hand) {
+            ranks.add(c.rank);
+        }
+        return ranks.size() == 1;
+    }
+
+
+
     public static void main(String[] args) {
         HashSet<Card> deck = createDeck();
         HashSet<HashSet<Card>> hands = createHands(deck);
-        hands = hands.stream().filter(Main::isFlush).collect(Collectors.toCollection(HashSet::new));
-        System.out.println(hands.size());
+
+        HashSet<HashSet<Card>> flushes = hands.stream().filter(Main::isFlush).collect(Collectors.toCollection(HashSet::new));
+        System.out.println(flushes.size());
+
+        HashSet<HashSet<Card>> straights = hands.stream().filter(Main::isStraight).collect(Collectors.toCollection(HashSet::new));
+        System.out.println(straights.size());
+
+        HashSet<HashSet<Card>> fours = hands.stream().filter(Main::fourOfAKind).collect(Collectors.toCollection(HashSet::new));
+        System.out.println(fours.size());
+
+        HashSet<HashSet<Card>> threes = hands.stream().filter(Main::threeOfAKind).collect(Collectors.toCollection(HashSet::new));
+        System.out.println(threes.size());
+
+        HashSet<HashSet<Card>> pairs = hands.stream().filter(Main::isPair).collect(Collectors.toCollection(HashSet::new));
+        System.out.println(pairs.size());
     }
 }
